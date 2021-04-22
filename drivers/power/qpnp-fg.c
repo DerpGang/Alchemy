db82156b2b397c84477bbfe33df97170d01c663e
Wei Wang <wvw@google.com>
2021-04-22 09:50:00 +0000

    ANDROID: fix uninitilized variable
    Currently we set CONFIG_CC_OPTIMIZE_FOR_SIZE which suppressed the compiler
    warning of unused variables which can lead undefined behavior e.g. memory
    corruption and panic. See https://lkml.org/lkml/2013/3/25/347.
    
    This patch fixes all the uninitilized variables in kernel
    
    Bug: 33353384
    Test: On device
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I0ae1082f447b435d71156d471878ba71aa16c378
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -3461,9 +3461,7 @@ static int estimate_battery_age(struct fg_chip *chip, int *actual_capacity)
 	}
 
 	battery_soc = get_battery_soc_raw(chip) * 100 / FULL_PERCENT_3B;
-	if (rc) {
-		goto error_done;
-	} else if (battery_soc < 25 || battery_soc > 75) {
+	if (battery_soc < 25 || battery_soc > 75) {
 		if (fg_debug_mask & FG_AGING)
 			pr_debug("Battery SoC (%d) out of range, aborting\n",
 					(int)battery_soc);
@@ -8943,7 +8941,6 @@ out:
 static int fg_memif_init(struct fg_chip *chip)
 {
 	int rc;
-	u8 dig_major;
 
 	rc = fg_read(chip, chip->revision, chip->mem_base + DIG_MINOR, 4);
 	if (rc) {
@@ -8961,7 +8958,7 @@ static int fg_memif_init(struct fg_chip *chip)
 		chip->ima_supported = true;
 		break;
 	default:
-		pr_err("Digital Major rev=%d not supported\n", dig_major);
+		pr_err("Digital Major rev=%d not supported\n", chip->revision[DIG_MAJOR]);
 		return -EINVAL;
 	}
 
